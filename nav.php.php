aca8f564815a6ba01367f9cd8faf8e4606113695
hirzaneq <hirza_neq@connect.ite.edu.sg>
2021-06-03 10:39:54 +0800

    completed pess

--- nav.php.php
+++ nav.php.php
@@ -1,5 +1,6 @@
 <h1>Police Emergency Service System</h1>
-
+<link href="header_style.css" rel="stylesheet" type="text/css">
+<link href="content_style.css" rel="stylesheet" type="text/css">
 <nav>
 
     <a href="logcall.php">Log Call</a>
